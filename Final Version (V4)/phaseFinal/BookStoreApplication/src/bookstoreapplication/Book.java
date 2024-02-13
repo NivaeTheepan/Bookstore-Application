23e42784ad1772717dc82285d08440dbd1ad9f4a
Nivaethan Piratheepan <130410644+NivaeTheepan@users.noreply.github.com>
2024-02-13 10:09:45 -0500

    Add files via upload

---
+++ Final Version (V4)/phaseFinal/BookStoreApplication/src/bookstoreapplication/Book.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstoreapplication;
+
+/**
+ *
+ * @author lalith
+ */
+public class Book {
+    private String title;
+    private double price;
+
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    // this is for testing purposes
+    @Override
+    public String toString() {
+        return "Title: " + title + "; Price: " + price;
+    }
+}
